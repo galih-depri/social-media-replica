f7c28991c103555c45211f2f7abbd941dda8fbab
Galih Depri <mr.lenggana@gmail.com>
2020-09-29 13:25:12 +0700

    Adding navbar and pages

--- sosmed_client/src/App.js
+++ sosmed_client/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+// Components
+import Navbar from "./components/Navbar";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#739574",
+      main: "#a5d6a7",
+      dark: "#b7deb8",
+      contrastText: "#000",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home}></Route>
+                <Route exact path="/login" component={login}></Route>
+                <Route exact path="/signup" component={signup}></Route>
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
